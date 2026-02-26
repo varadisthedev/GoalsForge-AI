fd1f5bab80082a65a45f60c839279236c91f3e63
varadisthedev <varadisthedev@gmail.com>
2026-02-27 00:02:51 +0530

    better error handing in psg call

--- index.ts
+++ index.ts
@@ -15,10 +15,16 @@ app.get("/",(req,res)=>{
     res.send("hello from bun + express")
 })
 
-app.get("/users",async (req,res)=>{
-    const resultData = await pool.query("SELECT * FROM USERS");
-    res.json(resultData);
-})
+app.get("/users", async (req, res) => {
+  try {
+    const resultData = await pool.query("SELECT * FROM users");
+    res.json({ resultData: resultData.rows, status: "success" });
+  } catch (err) {
+    console.error("DB ERROR FULL:", err);
+    res.status(500).json({ error: err });
+  }
+});
 app.listen(PORT||3000,()=>{
-    log(chalk.blue("server started at port: ",PORT))
+    log(chalk.gray("###############################"));
+    log(chalk.blue("server started at port: ",PORT));
 })
