945df13f84f5cc7c5b05aed522a9ac017be8e68e
varadisthedev <varadisthedev@gmail.com>
2026-02-27 00:02:37 +0530

    minor debugging

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,17 @@
 import {Pool} from "pg";
+import dotenv from "dotenv";
+import fs from "fs";
 
+dotenv.config();
+// if(fs.existsSync('.env')){
+  
+// }
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
 export const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
