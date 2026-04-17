bb31d653b20aea87166b37d577b229b16b50c427
varadisthedev <varadisthedev@gmail.com>
2026-04-17 23:34:30 +0530

    bug fixes in router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,18 @@
-import { useState, useMemo } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { useState, useMemo } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 function App() {
   // const [theme, setTheme] = useState("light");
 
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <h1>called app.jsx</h1>
-        </Route>
-
-        <Route path="/about" exact>
-          <h1>called about.jsx</h1>
-        </Route>
-
-
-      </Switch>
+      <Routes>
+        <Route path="/" element={<h1>called app.jsx</h1>} />
+        <Route path="/about" element={<h1>called about.jsx</h1>} />
+      </Routes>
     </Router>
   )
 }
 
-    </>
-  )
-}
 
 export default App
