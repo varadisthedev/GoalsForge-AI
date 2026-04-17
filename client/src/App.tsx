ed944a679274b69d5cf71605897d4b321a83b39c
varadisthedev <varadisthedev@gmail.com>
2026-04-17 23:19:00 +0530

    starting on frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,12 @@
 import { useState, useMemo } from 'react'
 
 import './App.css'
-import { List } from './components/List';
-
 function App() {
   const [theme, setTheme] = useState("light");
 
-  // const memoizedval = useMemo(() => {
-  //   console.log("using use memo for this espensive calculation");
-  //   return filterContent(arg1, arg2);
-  // }, [arg1, , arg2]); // depending
   return (
     <>
-      <div>
-        <h1>below botton swaps modes, dark and light</h1>
-        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")} className=''>click mode</button>
-
-        {/* importing list */}
-        <List />
-      </div>
+      <h1>called app.jsx</h1>
     </>
   )
 }
